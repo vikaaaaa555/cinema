b40f364b6e535e7fec9d32602b6006a32dd3d632
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-10 22:27:11 +0300

    add setupMenu and onDestroyView funcs

--- app/src/main/java/com/example/cinemaapp/screens/main/MainFragment.kt
+++ app/src/main/java/com/example/cinemaapp/screens/main/MainFragment.kt
@@ -1,19 +1,73 @@
 package com.example.cinemaapp.screens.main
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cinemaapp.MAIN
 import com.example.cinemaapp.R
+import com.example.cinemaapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    private var mBinding: FragmentMainBinding ?= null
+    private val binding get() = mBinding!!
+    lateinit var recyclerView: RecyclerView
+    private val adapter by lazy { MainAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        mBinding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+        setupMenu()
+    }
+
+    private fun init() {
+        val viewModel = ViewModelProvider(this)[MainFragmentViewModel::class.java]
+        viewModel.getMovies()
+        recyclerView = binding.recycleviewMain
+        recyclerView.adapter = adapter
+        try {
+            viewModel.myMovies.observe(viewLifecycleOwner) { list ->  //возможно на строчке снизу нужно не viewLifecycleOwner, а this
+                adapter.setList(list.body()!!.results)
+            }
+        } catch (e: Exception) {
+            Toast.makeText(MAIN, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mBinding = null
+    }
+
+    private fun setupMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onPrepareMenu(menu: Menu) {
+                // Handle for example visibility of menu items
+            }
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.main_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Validate and handle the selected menu item
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
 }
