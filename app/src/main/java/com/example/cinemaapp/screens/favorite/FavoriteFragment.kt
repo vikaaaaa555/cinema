f9a0fc6ab1a3347928bbda62b86b5a8268afcce9
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-11 22:56:32 +0300

    add onDestrot, onViewCreated and init funcs

--- app/src/main/java/com/example/cinemaapp/screens/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/cinemaapp/screens/favorite/FavoriteFragment.kt
@@ -5,15 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cinemaapp.R
+import com.example.cinemaapp.databinding.FragmentFavoriteBinding
+import com.example.cinemaapp.databinding.FragmentMainBinding
+import com.example.cinemaapp.screens.main.MainAdapter
+import com.example.cinemaapp.screens.main.MainFragmentViewModel
 
 class FavoriteFragment : Fragment() {
+
+    private var mBinding: FragmentFavoriteBinding?= null
+    private val binding get() = mBinding!!
+    lateinit var recyclerView: RecyclerView
+    private val adapter by lazy { MainAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        mBinding = FragmentFavoriteBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mBinding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        val viewModel = ViewModelProvider(this).get(FavoriteFragmentViewModel::class.java)
+        recyclerView = binding.recycleviewFavorite
+        recyclerView.adapter = adapter
     }
 }
