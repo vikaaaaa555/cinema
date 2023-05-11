f8af002b7429fce663f8efcadd4910a12f7a3aea
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-11 18:39:53 +0300

    add getMovies func

--- app/src/main/java/com/example/cinemaapp/screens/main/MainFragmentViewModel.kt
+++ app/src/main/java/com/example/cinemaapp/screens/main/MainFragmentViewModel.kt
@@ -1,6 +1,28 @@
 package com.example.cinemaapp.screens.main
 
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cinemaapp.MAIN
+import com.example.cinemaapp.data.retrofit.RetrofitRepository
+import com.example.cinemaapp.models.MoviesModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class MainFragmentViewModel: ViewModel() {
+    private val repository = RetrofitRepository()
+    val myMovies: MutableLiveData<Response<MoviesModel>> = MutableLiveData()
+
+    fun getMovies() {
+        viewModelScope.launch {
+            try {
+                myMovies.value = repository.getMovies()
+            } catch (e: Exception) {
+                Log.e("Error", e.message.toString())
+                //Toast.makeText(MAIN, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
