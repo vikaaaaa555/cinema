69504a02ec5a3dc0d34c17cba82cd63087af27a3
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-18 23:12:15 +0300

    add getMovies fun

--- app/src/main/java/com/example/cinemaapp/data/retrofit/RetrofitRepository.kt
+++ app/src/main/java/com/example/cinemaapp/data/retrofit/RetrofitRepository.kt
@@ -1,4 +1,11 @@
 package com.example.cinemaapp.data.retrofit
 
+import com.example.cinemaapp.data.retrofit.api.RetrofitInstance
+import com.example.cinemaapp.models.MoviesModel
+import retrofit2.Response
+
 class RetrofitRepository {
+    suspend fun getMovies(): Response<MoviesModel> {
+        return RetrofitInstance.api.getPopularMovies()
+    }
 }
