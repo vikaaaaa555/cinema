912afeb94b4b6c244880dd6e1de64f217fab6d73
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-18 23:37:21 +0300

    realization insert & delete

--- app/src/main/java/com/example/cinemaapp/screens/detail/DetailFragmentViewModel.kt
+++ app/src/main/java/com/example/cinemaapp/screens/detail/DetailFragmentViewModel.kt
@@ -1,6 +1,23 @@
 package com.example.cinemaapp.screens.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cinemaapp.REALIZATION
+import com.example.cinemaapp.models.MovieItemModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class DetailFragmentViewModel: ViewModel() {
+    fun insert(movieItemModel: MovieItemModel, onSuccess:() -> Unit) =
+        viewModelScope.launch (Dispatchers.IO) {
+            REALIZATION.insertMovie(movieItemModel) {
+                onSuccess()
+            }
+        }
+    fun delete(movieItemModel: MovieItemModel, onSuccess:() -> Unit) =
+        viewModelScope.launch (Dispatchers.IO) {
+            REALIZATION.deleteMovie(movieItemModel) {
+                onSuccess()
+            }
+        }
 }
