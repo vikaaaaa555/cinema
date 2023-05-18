36fbf9e9a2d3dc55ad807fa024de4eae204723b6
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-18 23:47:57 +0300

    add getAllMovies func

--- app/src/main/java/com/example/cinemaapp/screens/favorite/FavoriteFragmentViewModel.kt
+++ app/src/main/java/com/example/cinemaapp/screens/favorite/FavoriteFragmentViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.cinemaapp.screens.favorite
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.cinemaapp.REALIZATION
+import com.example.cinemaapp.data.room.repository.MoviesRepositoryRealization
+import com.example.cinemaapp.models.MovieItemModel
 
 class FavoriteFragmentViewModel: ViewModel() {
+    fun getAllMovies(): LiveData<List<MovieItemModel>> {
+        return REALIZATION.allMovies
+    }
 }
