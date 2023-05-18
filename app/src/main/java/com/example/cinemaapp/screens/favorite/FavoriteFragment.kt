acdd43bdcdbdf24c3fe19e090f98d0ce919cc50a
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-18 23:43:30 +0300

    add clickMovie func

--- app/src/main/java/com/example/cinemaapp/screens/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/cinemaapp/screens/favorite/FavoriteFragment.kt
@@ -1,24 +1,28 @@
 package com.example.cinemaapp.screens.favorite
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cinemaapp.MAIN
 import com.example.cinemaapp.R
 import com.example.cinemaapp.databinding.FragmentFavoriteBinding
-import com.example.cinemaapp.databinding.FragmentMainBinding
+import com.example.cinemaapp.models.MovieItemModel
 import com.example.cinemaapp.screens.main.MainAdapter
-import com.example.cinemaapp.screens.main.MainFragmentViewModel
 
 class FavoriteFragment : Fragment() {
 
     private var mBinding: FragmentFavoriteBinding?= null
     private val binding get() = mBinding!!
     lateinit var recyclerView: RecyclerView
-    private val adapter by lazy { MainAdapter() }
+    private val adapter by lazy { FavoriteAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,8 +43,19 @@ class FavoriteFragment : Fragment() {
     }
 
     private fun init() {
-        val viewModel = ViewModelProvider(this).get(FavoriteFragmentViewModel::class.java)
+        val viewModel = ViewModelProvider(this)[FavoriteFragmentViewModel::class.java]
         recyclerView = binding.recycleviewFavorite
         recyclerView.adapter = adapter
+        viewModel.getAllMovies().observe(viewLifecycleOwner) { list ->
+            adapter.setList(list.asReversed())
+        }
+    }
+
+    companion object {
+        fun clickMovie(model: MovieItemModel) {
+            val bundle = Bundle()
+            bundle.putSerializable("movie", model)
+            MAIN.navController.navigate(R.id.action_favoriteFragment_to_detailFragment, bundle)
+        }
     }
 }
