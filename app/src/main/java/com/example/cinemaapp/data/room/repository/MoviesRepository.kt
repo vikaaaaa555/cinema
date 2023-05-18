570563007cba36e1ee5a6af1105ffbe431808fda
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-18 23:14:06 +0300

    add insert & delete functions

--- app/src/main/java/com/example/cinemaapp/data/room/repository/MoviesRepository.kt
+++ app/src/main/java/com/example/cinemaapp/data/room/repository/MoviesRepository.kt
@@ -1,4 +1,11 @@
 package com.example.cinemaapp.data.room.repository
 
+import androidx.lifecycle.LiveData
+import com.example.cinemaapp.models.MovieItemModel
+
 interface MoviesRepository {
+    val allMovies: LiveData<List<MovieItemModel>>
+
+    suspend fun insertMovie(movieItemModel: MovieItemModel, onSuccsess:() -> Unit)
+    suspend fun deleteMovie(movieItemModel: MovieItemModel, onSuccsess:() -> Unit)
 }
