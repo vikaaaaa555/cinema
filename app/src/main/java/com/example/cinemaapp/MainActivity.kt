6c313dfa46006c804acb27edeb7df93257e85359
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-18 23:56:30 +0300

    fix bugs

--- app/src/main/java/com/example/cinemaapp/MainActivity.kt
+++ app/src/main/java/com/example/cinemaapp/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.cinemaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.cinemaapp.databinding.ActivityMainBinding
+import com.example.cinemaapp.screens.main.MainMenuProvider
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding?= null
+    private val binding get() = mBinding!!
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         MAIN = this
+        navController = Navigation.findNavController(this, R.id.nav_host)
+
+        // Создаем новый экземпляр класса MyMenuProvider
+        val menuProvider = MainMenuProvider()
+        // Добавляем его в качестве провайдера меню
+        addMenuProvider(menuProvider)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
