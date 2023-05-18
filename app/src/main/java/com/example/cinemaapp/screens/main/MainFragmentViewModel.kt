7a71a311af85ea5fbc80e56f62f9c45c82098f58
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-18 23:50:18 +0300

    add initDatabase

--- app/src/main/java/com/example/cinemaapp/screens/main/MainFragmentViewModel.kt
+++ app/src/main/java/com/example/cinemaapp/screens/main/MainFragmentViewModel.kt
@@ -1,28 +1,35 @@
 package com.example.cinemaapp.screens.main
 
+import android.app.Application
 import android.util.Log
-import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.cinemaapp.MAIN
+import com.example.cinemaapp.REALIZATION
 import com.example.cinemaapp.data.retrofit.RetrofitRepository
+import com.example.cinemaapp.data.room.MoviesRoomDatabase
+import com.example.cinemaapp.data.room.repository.MoviesRepositoryRealization
 import com.example.cinemaapp.models.MoviesModel
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
-class MainFragmentViewModel: ViewModel() {
+class MainFragmentViewModel(application: Application): AndroidViewModel(application) {
     private val repository = RetrofitRepository()
     val myMovies: MutableLiveData<Response<MoviesModel>> = MutableLiveData()
+    val context = application
 
-    fun getMovies() {
+    fun getMoviesRetrofit() {
         viewModelScope.launch {
             try {
                 myMovies.value = repository.getMovies()
             } catch (e: Exception) {
                 Log.e("Error", e.message.toString())
-                //Toast.makeText(MAIN, e.message, Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    fun initDatabase() {
+        val daoMovie = MoviesRoomDatabase.getInstance(context).getMovieDao()
+        REALIZATION = MoviesRepositoryRealization(daoMovie)
     }
 }
