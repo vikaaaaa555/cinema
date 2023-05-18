dea7f68420264ff71953d4744b1324cf4a29dd78
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-18 23:13:07 +0300

    realization DAO interface

--- app/src/main/java/com/example/cinemaapp/data/room/dao/MoviesDao.kt
+++ app/src/main/java/com/example/cinemaapp/data/room/dao/MoviesDao.kt
@@ -1,4 +1,18 @@
 package com.example.cinemaapp.data.room.dao
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.cinemaapp.models.MovieItemModel
+
+@Dao
 interface MoviesDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(movieItemModel: MovieItemModel)
+
+    @Delete
+    suspend fun delete(movieItemModel: MovieItemModel)
+
+    @Query("SELECT * from movie_table")
+    fun getAllMovies() : LiveData<List<MovieItemModel>>
 }
