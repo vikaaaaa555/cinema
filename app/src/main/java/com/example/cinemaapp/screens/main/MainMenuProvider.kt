ce8ba1b81aef0db5d121c84b4535d6fbe0d3b548
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-18 09:38:51 +0300

    create onMenuItemSelected

--- app/src/main/java/com/example/cinemaapp/screens/main/MainMenuProvider.kt
+++ app/src/main/java/com/example/cinemaapp/screens/main/MainMenuProvider.kt
@@ -1,33 +1,25 @@
 package com.example.cinemaapp.screens.main
 
-import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuProvider
+import com.example.cinemaapp.MAIN
 import com.example.cinemaapp.R
 
-class MyMenuProvider : MenuProvider {
+class MainMenuProvider : MenuProvider {
+
     override fun onCreateMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.main_menu, menu)
     }
 
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        // Обработка выбранного пункта меню
-        return true
+        return when (menuItem.itemId) {
+            R.id.item_favorite -> {
+                MAIN.navController.navigate(R.id.action_mainFragment_to_favoriteFragment)
+                true
+            }
+            else -> false
+        }
     }
 }
-/*
-class MyActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Создаем новый экземпляр класса MyMenuProvider
-        val menuProvider = MyMenuProvider()
-
-        // Добавляем его в качестве провайдера меню
-        addMenuProvider(menuProvider)
-    }
-}*/
