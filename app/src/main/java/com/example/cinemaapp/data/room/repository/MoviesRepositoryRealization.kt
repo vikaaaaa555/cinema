7a2b678a29dcb60dbc1019fb8990b42652851d13
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-18 23:15:19 +0300

    realization interface methods

--- app/src/main/java/com/example/cinemaapp/data/room/repository/MoviesRepositoryRealization.kt
+++ app/src/main/java/com/example/cinemaapp/data/room/repository/MoviesRepositoryRealization.kt
@@ -1,4 +1,20 @@
 package com.example.cinemaapp.data.room.repository
 
-class MoviesRepositoryRealization {
+import androidx.lifecycle.LiveData
+import com.example.cinemaapp.data.room.dao.MoviesDao
+import com.example.cinemaapp.models.MovieItemModel
+
+class MoviesRepositoryRealization(private val moviesDao: MoviesDao): MoviesRepository {
+    override val allMovies: LiveData<List<MovieItemModel>>
+        get() = moviesDao.getAllMovies()
+
+    override suspend fun insertMovie(movieItemModel: MovieItemModel, onSuccsess: () -> Unit) {
+        moviesDao.insert(movieItemModel)
+        onSuccsess()
+    }
+
+    override suspend fun deleteMovie(movieItemModel: MovieItemModel, onSuccsess: () -> Unit) {
+        moviesDao.delete(movieItemModel)
+        onSuccsess()
+    }
 }
