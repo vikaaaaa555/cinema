75fed545d7d47a08e76d39120f49dd92492fdf6f
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-18 23:19:41 +0300

    inicialization database

--- app/src/main/java/com/example/cinemaapp/data/room/MoviesRoomDatabase.kt
+++ app/src/main/java/com/example/cinemaapp/data/room/MoviesRoomDatabase.kt
@@ -1,4 +1,29 @@
-package com.example.cinemaapp.data.room.repository
+package com.example.cinemaapp.data.room
 
-class MoviesRoomDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.cinemaapp.data.room.dao.MoviesDao
+import com.example.cinemaapp.models.MovieItemModel
+
+@Database(entities = [MovieItemModel::class], version = 1)
+abstract class MoviesRoomDatabase: RoomDatabase() {
+
+    abstract fun getMovieDao(): MoviesDao
+
+    companion object {
+        private var database: MoviesRoomDatabase ?= null
+
+        fun getInstance(context: Context): MoviesRoomDatabase {
+            return if (database == null) {
+                database = Room
+                    .databaseBuilder(context, MoviesRoomDatabase::class.java, "database")
+                    .build()
+                database as MoviesRoomDatabase
+            } else {
+                database as MoviesRoomDatabase
+            }
+        }
+    }
 }
