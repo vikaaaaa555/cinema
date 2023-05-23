abc8233465138691ce9b25c90fd84cdca034ea2c
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-23 18:41:38 +0300

    added background poster display & correct work with the database

--- app/src/main/java/com/example/cinemaapp/screens/detail/DetailFragment.kt
+++ app/src/main/java/com/example/cinemaapp/screens/detail/DetailFragment.kt
@@ -7,11 +7,10 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
-import com.example.cinemaapp.MAIN
-import com.example.cinemaapp.POSTER_PATH
-import com.example.cinemaapp.R
+import com.example.cinemaapp.*
 import com.example.cinemaapp.databinding.FragmentDetailBinding
 import com.example.cinemaapp.models.MovieItemModel
 import com.example.cinemaapp.screens.favorite.FavoriteFragmentViewModel
@@ -37,28 +36,53 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Очищаем пользовательское представление ActionBar
+        (requireActivity() as AppCompatActivity).supportActionBar?.customView = null
+        // Показываем название приложения в ActionBar
+        (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(true)
+
         init()
     }
 
     private fun init() {
+        val valueBool = SaveShared.getFavorite(MAIN, currentMovie.id.toString())
         val viewModel = ViewModelProvider(this)[DetailFragmentViewModel::class.java]
+
+        if (isFavorite != valueBool) {
+            binding.imageDetailFavorite.setImageResource(R.drawable.baseline_favorite_24)
+        } else {
+            binding.imageDetailFavorite.setImageResource(R.drawable.baseline_favorite_border_24)
+        }
+
+        binding.tvTitle.text = currentMovie.title
+        binding.tvDate.text = currentMovie.release_date
+        binding.tvOverride.text = currentMovie.overview
+        binding.tvRatingBar.rating = currentMovie.vote_average.toFloat()
 
         Glide.with(MAIN)
             .load("$POSTER_PATH${currentMovie.poster_path}")
             .centerCrop()
             .placeholder(R.drawable.ic_launcher_foreground)
             .into(binding.imageDetail)
-        binding.tvTitle.text = currentMovie.title
-        binding.tvDate.text = currentMovie.release_date
-        binding.tvDescrition.text = currentMovie.overview
+
+        Glide.with(MAIN)
+            .load("$BACKDROP_PATH${currentMovie.backdrop_path}")
+            .centerCrop()
+            .placeholder(R.drawable.ic_launcher_foreground)
+            .into(binding.movieBackdrop)
+
+
 
         binding.imageDetailFavorite.setOnClickListener {
-            isFavorite = if (!isFavorite) {
+            isFavorite = if (isFavorite == valueBool) {
                 binding.imageDetailFavorite.setImageResource(R.drawable.baseline_favorite_24)
+                SaveShared.setFavorite(MAIN, currentMovie.id.toString(), true)
                 viewModel.insert(currentMovie) {}
                 true
             } else {
                 binding.imageDetailFavorite.setImageResource(R.drawable.baseline_favorite_border_24)
+                SaveShared.setFavorite(MAIN, currentMovie.id.toString(), false)
                 viewModel.delete(currentMovie) {}
                 false
             }
