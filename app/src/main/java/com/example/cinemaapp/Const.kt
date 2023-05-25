80fb937c57e87fa4c2fb3128d5f9bc26fa9ee25c
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-25 09:23:01 +0300

    added BACKDROP_PATH, YOUTUBE_BASE_URL & YOUTUBE_API_KEY

--- app/src/main/java/com/example/cinemaapp/Const.kt
+++ app/src/main/java/com/example/cinemaapp/Const.kt
@@ -3,7 +3,11 @@ package com.example.cinemaapp
 import com.example.cinemaapp.data.room.repository.MoviesRepositoryRealization
 
 lateinit var MAIN: MainActivity
-val API_KEY = "12a014be11f3867f04241a38e03a2f57"
-const val BASE_URL = "https://api.themoviedb.org/"
+const val API_KEY = "12a014be11f3867f04241a38e03a2f57"
+const val YOUTUBE_API_KEY = "AIzaSyBwQ64MFIdN11nIKOAeDIV8V6cw5gm96gY"
+const val BASE_URL = "https://api.themoviedb.org/3/"
+const val YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/"
 const val POSTER_PATH = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2"
+const val BACKDROP_PATH = "https://www.themoviedb.org/t/p/w1280"
 lateinit var REALIZATION: MoviesRepositoryRealization
+lateinit var TITTLE_NAME: String
