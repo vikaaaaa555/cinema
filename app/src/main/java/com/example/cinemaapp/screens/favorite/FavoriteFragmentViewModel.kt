e66c7063afff95a9baa2d06a2239e28f70aa2b3f
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-25 09:26:26 +0300

    imported movieItemModel

--- app/src/main/java/com/example/cinemaapp/screens/favorite/FavoriteFragmentViewModel.kt
+++ app/src/main/java/com/example/cinemaapp/screens/favorite/FavoriteFragmentViewModel.kt
@@ -3,8 +3,7 @@ package com.example.cinemaapp.screens.favorite
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.cinemaapp.REALIZATION
-import com.example.cinemaapp.data.room.repository.MoviesRepositoryRealization
-import com.example.cinemaapp.models.MovieItemModel
+import com.example.cinemaapp.models.movies.MovieItemModel
 
 class FavoriteFragmentViewModel: ViewModel() {
     fun getAllMovies(): LiveData<List<MovieItemModel>> {
