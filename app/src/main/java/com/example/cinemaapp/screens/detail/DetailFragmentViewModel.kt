1686c3da3ed96385e2c0e6da1934fa63c613cc5e
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-25 09:28:45 +0300

    added trailer view methods

--- app/src/main/java/com/example/cinemaapp/screens/detail/DetailFragmentViewModel.kt
+++ app/src/main/java/com/example/cinemaapp/screens/detail/DetailFragmentViewModel.kt
@@ -2,22 +2,43 @@ package com.example.cinemaapp.screens.detail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.cinemaapp.API_KEY
 import com.example.cinemaapp.REALIZATION
-import com.example.cinemaapp.models.MovieItemModel
+import com.example.cinemaapp.YOUTUBE_API_KEY
+import com.example.cinemaapp.data.retrofit.RetrofitRepository
+import com.example.cinemaapp.models.movies.MovieItemModel
+import com.example.cinemaapp.models.trailers.TrailerModel
+import com.example.cinemaapp.models.youtube.YouTubeVideoDetailsResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Response
 
 class DetailFragmentViewModel: ViewModel() {
+    private val repository = RetrofitRepository()
+
     fun insert(movieItemModel: MovieItemModel, onSuccess:() -> Unit) =
         viewModelScope.launch (Dispatchers.IO) {
             REALIZATION.insertMovie(movieItemModel) {
                 onSuccess()
             }
         }
+
     fun delete(movieItemModel: MovieItemModel, onSuccess:() -> Unit) =
         viewModelScope.launch (Dispatchers.IO) {
             REALIZATION.deleteMovie(movieItemModel) {
                 onSuccess()
             }
         }
+
+    suspend fun getMovieTrailers(movieId: Int): Response<TrailerModel> {
+        return withContext(viewModelScope.coroutineContext) {
+            repository.getMovieTrailers(movieId, API_KEY, "en-US")
+        }
+    }
+
+    suspend fun getVideoDetails(videoId: String): YouTubeVideoDetailsResponse {
+        val apiKey = YOUTUBE_API_KEY
+        return repository.getVideoDetails(videoId, apiKey)
+    }
 }
