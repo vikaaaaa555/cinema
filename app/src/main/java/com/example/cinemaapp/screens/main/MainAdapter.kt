eb4ba66f5e63d96bbbb2ecb5ff02ed60cd267d95
Victoriya Zviahintsava <vika.zv04@yandex.ru>
2023-05-25 09:25:42 +0300

    added rating

--- app/src/main/java/com/example/cinemaapp/screens/main/MainAdapter.kt
+++ app/src/main/java/com/example/cinemaapp/screens/main/MainAdapter.kt
@@ -4,13 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.cinemaapp.MAIN
 import com.example.cinemaapp.POSTER_PATH
 import com.example.cinemaapp.R
-import com.example.cinemaapp.models.MovieItemModel
+import com.example.cinemaapp.models.movies.MovieItemModel
 
 class MainAdapter: RecyclerView.Adapter<MainAdapter.MyViewHolder>() {
     private var listMovies = emptyList<MovieItemModel>()
@@ -19,6 +20,7 @@ class MainAdapter: RecyclerView.Adapter<MainAdapter.MyViewHolder>() {
         val titleTextView: TextView = itemView.findViewById(R.id.item_title)
         val dateTextView: TextView = itemView.findViewById(R.id.item_date)
         val myImageView: ImageView = itemView.findViewById(R.id.item_image)
+        val ratingBarView: RatingBar = itemView.findViewById(R.id.item_rating_bar)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
@@ -28,6 +30,7 @@ class MainAdapter: RecyclerView.Adapter<MainAdapter.MyViewHolder>() {
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.titleTextView.text = listMovies[position].title
         holder.dateTextView.text = listMovies[position].release_date
+        holder.ratingBarView.rating = listMovies[position].vote_average.toFloat()
 
         Glide.with(MAIN)
             .load("${POSTER_PATH}${listMovies[position].poster_path}")
